c2ec9543efe827d501187f5406cb5ecf27e9c418
HILL <1015599722@qq.com>
2020-02-15 15:24:40 +0800

    完成日志系统配置，实现分级别文件记录

--- counsel/src/main/java/org/gdou/controller/IndexController.java
+++ counsel/src/main/java/org/gdou/controller/IndexController.java
@@ -1,5 +1,6 @@
 package org.gdou.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @date 2020/2/6
  **/
 @Controller
+@Slf4j
 public class IndexController {
 
     @RequestMapping("/index")
     @ResponseBody
     public String index(){
+
         return "helloWorld！！";
     }
 
