a971d735356fedd89c3d7b189b967598a4cfde7e
HILL <1015599722@qq.com>
2020-03-29 22:28:09 +0800

    1.新增

--- counsel/src/main/java/org/gdou/controller/TestController.java
+++ counsel/src/main/java/org/gdou/controller/TestController.java
@@ -6,6 +6,7 @@ import org.gdou.common.utils.RedisUtil;
 import org.gdou.model.po.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author HILL
@@ -18,13 +19,22 @@ public class TestController {
     @Autowired
     RedisUtil redisUtil;
 
+    @RequestMapping("/set")
     public Result redisSpedTest(){
         long start = System.currentTimeMillis();
         User user =new User();
         user.setName("许俊锋"+start);
         redisUtil.set("test",user);
+        long end = System.currentTimeMillis();
+        return ResultGenerator.genSuccessResult((start-end)+"ms");
+    }
+
+    @RequestMapping("/get")
+    public Result redisGetTest(){
+        long start = System.currentTimeMillis();
         Object test = redisUtil.get("test");
         long end = System.currentTimeMillis();
         return ResultGenerator.genSuccessResult((start-end)+"ms");
     }
+
 }
