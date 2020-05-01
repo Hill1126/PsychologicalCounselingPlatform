f6ca21b7b52df66a21442dbaaa12c90ac5a6d628
HILL <1015599722@qq.com>
2020-05-01 09:31:41 +0800

    【feature】添加跨域配置

--- counsel/src/main/java/org/gdou/config/ApiWebMvcConfig.java
+++ counsel/src/main/java/org/gdou/config/ApiWebMvcConfig.java
@@ -6,6 +6,7 @@ import org.gdou.common.interceptor.SystemLogInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -50,4 +51,14 @@ public class ApiWebMvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/static/**");
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("*");
+
+
+    }
 }
