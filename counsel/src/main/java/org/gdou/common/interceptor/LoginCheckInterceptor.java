9ade7d8168dafe48c1ff08235539b26c53256caa
HILL <1015599722@qq.com>
2020-05-13 11:18:32 +0800

    【change】抽取验证错误方法

--- counsel/src/main/java/org/gdou/common/interceptor/LoginCheckInterceptor.java
+++ counsel/src/main/java/org/gdou/common/interceptor/LoginCheckInterceptor.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Arrays;
 
 /**
@@ -26,11 +27,7 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         var user = UserUtils.getUserByToken(request);
         if (user==null){
-            response.setContentType("text/json;charset=UTF-8");
-            Result result = new Result();
-            result.setMessage("请登陆后操作");
-            result.setCode(ResultCode.UNAUTHORIZED);
-            response.getWriter().write(result.toString());
+            writeUnauthorizedResult(response, "请登陆后操作");
             return false;
         }
         //检查注解是否需要权限校验。
@@ -48,11 +45,7 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
             boolean hasRole = Arrays.stream(roleControl.userType()).anyMatch((roleCode) -> userType.equals(roleCode));
             //如果没有权限
             if (!hasRole){
-                response.setContentType("text/json;charset=UTF-8");
-                Result result = new Result();
-                result.setMessage("无权限操作");
-                result.setCode(ResultCode.UNAUTHORIZED);
-                response.getWriter().write(result.toString());
+                writeUnauthorizedResult(response, "无权限操作");
                 return false;
             }
         }
@@ -62,6 +55,13 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
         return true;
     }
 
+    private void writeUnauthorizedResult(HttpServletResponse response, String msg) throws IOException {
+        response.setContentType("text/json;charset=UTF-8");
+        Result result = new Result();
+        result.setMessage(msg);
+        result.setCode(ResultCode.UNAUTHORIZED);
+        response.getWriter().write(result.toString());
+    }
 
 
 }
