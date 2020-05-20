58e3e3a7612bac47a3a5b86879ee57659e474eae
HILL <1015599722@qq.com>
2020-05-20 22:07:37 +0800

    【fix】修复跨域问题

--- counsel/src/main/java/org/gdou/config/ApiWebMvcConfig.java
+++ counsel/src/main/java/org/gdou/config/ApiWebMvcConfig.java
@@ -6,7 +6,6 @@ import org.gdou.common.interceptor.SystemLogInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -53,21 +52,4 @@ public class ApiWebMvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/static/**");
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowCredentials(true)
-                .allowedHeaders("*")
-                .allowedMethods("*")
-                .allowedOrigins("*")
-                .exposedHeaders("Access-Control-Allow-Origin")
-                .exposedHeaders("Content-Type")
-                .exposedHeaders( "X-Requested-With")
-                .exposedHeaders("accept")
-                .exposedHeaders("Origin")
-                .exposedHeaders( "Access-Control-Request-Method")
-                .exposedHeaders("Access-Control-Request-Headers")
-                .maxAge(1000L*60*60);
-
-    }
 }
